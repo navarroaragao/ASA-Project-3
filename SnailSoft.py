0f773a7ead026e46fe71ee9df8f10be526e219f6
Manuel Soares <manuel.r.soares@tecnico.ulisboa.pt>
2025-12-26 16:17:31 +0000

    time optimized

--- SnailSoft.py
+++ SnailSoft.py
@@ -51,61 +51,56 @@ def calculate_min_wins(n, target_team, points, games):
     return result
 
 def can_win_with_wins(n, target_team, initial_points, games, num_wins, target_remaining_matches):
-
-    points = initial_points.copy()
     
     if num_wins > len(target_remaining_matches):
         return False
     
-    max_target_points = points[target_team] + num_wins * 3 + (len(target_remaining_matches) - num_wins) * 1
+    max_target_points = initial_points[target_team] + num_wins * 3 + (len(target_remaining_matches) - num_wins)
     
     for team in range(1, n + 1):
-        if team != target_team and points[team] > max_target_points:
+        if team != target_team and initial_points[team] > max_target_points:
             return False
+    
+    other_matches = []
+    for i in range(1, n + 1):
+        if i == target_team:
+            continue
+        for j in range(1, n + 1):
+            if j != i and j != target_team and not games[i][j]:
+                other_matches.append((i, j))
     
     prob = pulp.LpProblem("SnailSoft", pulp.LpMinimize)
     
-    x = {}
-    e = {}
+    x = {match: pulp.LpVariable(f"x_{match[0]}_{match[1]}", cat=pulp.LpBinary) 
+         for match in other_matches}
+    e = {match: pulp.LpVariable(f"e_{match[0]}_{match[1]}", cat=pulp.LpBinary) 
+         for match in other_matches}
     
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            if i != j and i != target_team and j != target_team and not games[i][j]:
-                x[(i, j)] = pulp.LpVariable(f"x_{i}_{j}", cat=pulp.LpBinary)
-                e[(i, j)] = pulp.LpVariable(f"e_{i}_{j}", cat=pulp.LpBinary)
+    y = {match: pulp.LpVariable(f"y_{match[0]}_{match[1]}", cat=pulp.LpBinary) 
+         for match in target_remaining_matches}
+    d = {match: pulp.LpVariable(f"d_{match[0]}_{match[1]}", cat=pulp.LpBinary) 
+         for match in target_remaining_matches}
     
-    y = {}  
-    d = {} 
-    
-    for match in target_remaining_matches:
-        y[match] = pulp.LpVariable(f"y_{match[0]}_{match[1]}", cat=pulp.LpBinary)
-        d[match] = pulp.LpVariable(f"d_{match[0]}_{match[1]}", cat=pulp.LpBinary)
-    
-    prob += pulp.lpSum([y[match] for match in target_remaining_matches]) == num_wins
+    prob += pulp.lpSum(y.values()) == num_wins
     
     for match in target_remaining_matches:
         prob += y[match] + d[match] <= 1
     
-    for (i, j) in x.keys():
-        prob += x[(i, j)] + e[(i, j)] <= 1
+    for match in other_matches:
+        prob += x[match] + e[match] <= 1
     
     for team in range(1, n + 1):
         if team == target_team:
             continue
         
-        team_points = points[team]
-        variables_added = False
+        team_points = initial_points[team]
         
         for match in target_remaining_matches:
             home, away = match
             if home == target_team and away == team:
-                
                 team_points += 3 * (1 - y[match] - d[match]) + d[match]
-                variables_added = True
             elif home == team and away == target_team:
-                
                 team_points += 3 * (1 - y[match] - d[match]) + d[match]
-                variables_added = True
         
         for other in range(1, n + 1):
             if other == team or other == target_team:
@@ -113,21 +108,15 @@ def can_win_with_wins(n, target_team, initial_points, games, num_wins, target_re
             
             if not games[team][other]:
                 team_points += 3 * x[(team, other)] + e[(team, other)]
-                variables_added = True
             
             if not games[other][team]:
                 team_points += 3 * (1 - x[(other, team)] - e[(other, team)]) + e[(other, team)]
-                variables_added = True
         
-        if not variables_added:
-            if team_points > max_target_points:
-                return False
-        else:
-            prob += team_points <= max_target_points
+        prob += team_points <= max_target_points
     
     prob += 0
     
-    prob.solve(pulp.PULP_CBC_CMD(msg=0))
+    prob.solve(pulp.PULP_CBC_CMD(msg=0, timeLimit=10, gapRel=0.01))
     
     return prob.status == pulp.LpStatusOptimal
 
